7833c1eea322d8b774cc61325330f9de1a9bbf41
xcrx <xcrx@gmail.com>
2013-10-23 14:20:59 -0500

    10-22-13

--- dbConnection.py
+++ dbConnection.py
@@ -1,10 +1,12 @@
 from PyQt4 import QtCore, QtGui, QtSql
+
+
 def defaultConnection():
     QtSql.QSqlDatabase.database('qt_sql_default_connection').close()
     QtSql.QSqlDatabase.removeDatabase('qt_sql_default_connection')
     db = QtSql.QSqlDatabase.addDatabase("QMYSQL")
     db.setUserName('rhanson')
-    passwd, exit = QtGui.QInputDialog.getText(None, 'Enter Password', 'Password',2)
+    passwd, exit = QtGui.QInputDialog.getText(None, 'Enter Password', 'Password', 2)
     db.setPassword(passwd)
     db.setHostName('192.168.1.79')
     db.setDatabaseName('tmh_fabrication')
@@ -13,7 +15,8 @@ def defaultConnection():
     else:
         dbErr(db)
         return False
-        
+
+
 def dbErr(qry):
     '''This error is used extensively.
     '''
